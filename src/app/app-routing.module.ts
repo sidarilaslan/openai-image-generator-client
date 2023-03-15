22282c67dcb6a8aa6a183d0d0991420948676078
sidar <ilaslansidar@gmail.com>
2023-03-16 00:27:44 +0300

    feat: components and modules have been created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  },
+  { path: "history", loadChildren: () => import('./components/history/history.module').then(module => module.HistoryModule) },
+  { path: "**", component: PageNotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/history/history.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HistoryComponent } from './history.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    HistoryComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: "", component: HistoryComponent }
+    ])
+  ],
+})
+export class HistoryModule { }
